33904fe130d26abcd44e88e89da05b4091e692c0
Andrei Shabanski <shabanski.andrei@gmail.com>
2022-09-02 17:15:08 +0300

    feat: implement ad-hoc `sort` command

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+## [0.1.0b3] - 2022-09-02
+
+### Added
+
+- Added `poetry sort` command to sort dependencies.
+
+## [0.1.0b2] - 2022-09-01
+
+### Added
+
+- Implemented dependencies sorting after calling `poetry add`, `poetry remove`, and `poetry init` commands.
--- poetry_plugin_sort/plugins.py
+++ poetry_plugin_sort/plugins.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
+from typing import Type
+
 from cleo.events import console_events
 from cleo.events.console_command_event import ConsoleCommandEvent
 from cleo.events.event_dispatcher import EventDispatcher
 from cleo.io.io import IO
 from poetry.console.application import Application
 from poetry.console.commands.add import AddCommand
+from poetry.console.commands.command import Command
 from poetry.console.commands.init import InitCommand
 from poetry.console.commands.remove import RemoveCommand
 from poetry.plugins.application_plugin import ApplicationPlugin
@@ -11,13 +16,29 @@ from poetry.plugins.application_plugin import ApplicationPlugin
 from poetry_plugin_sort.sort import Sorter
 
 
+class SortCommand(Command):
+    name = "sort"
+    description = "Sorts the dependencies in pyproject.toml"
+
+    def handle(self) -> int:
+        sorter = Sorter(self.poetry, self.io)
+        sorter.sort()
+        return 0
+
+
 class SortDependenciesPlugin(ApplicationPlugin):
     """Sorts dependencies in pyproject.toml file"""
 
-    def activate(self, application: Application):
+    @property
+    def commands(self) -> list[Type[Command]]:
+        return [SortCommand]
+
+    def activate(self, application: Application) -> None:
         application.event_dispatcher.add_listener(
             console_events.TERMINATE, self.sort_dependencies
         )
+
+        super().activate(application)
 
     def sort_dependencies(
         self, event: ConsoleCommandEvent, event_name: str, dispatcher: EventDispatcher
